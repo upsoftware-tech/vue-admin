141a66471b7dc9ac5c6743010616010a519e9274
4multimedia <biuro@4multimedia.pl>
2024-08-05 22:10:16 +0200

    1.0.7

--- src/router/admin.js
+++ src/router/admin.js
@@ -1,21 +1,19 @@
-const AdminRoutes = [
-	{
-		path: 'admin',
-		component: () => import('./../layouts/AdminLayout.vue'),
-		meta: {
-			title: 'Panel administracyjny'
-		},
-		children: [
-			{
-				path: '/',
-				component: () => import('./../pages/dashboard/IndexPage.vue'),
-				meta: {
-					title: 'Kokpit'
-				},
-			}
-		]
-	}
-];
+const AdminRoutes = {
+	path: 'admin',
+	component: () => import('./../layouts/AdminLayout.vue'),
+	meta: {
+		title: 'Panel administracyjny'
+	},
+	children: [
+		{
+			path: '/',
+			component: () => import('./../pages/dashboard/IndexPage.vue'),
+			meta: {
+				title: 'Kokpit'
+			},
+		}
+	]
+};
 
 export { AdminRoutes };
 
