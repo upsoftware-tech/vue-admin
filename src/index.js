3d2ed2c8b791ed4300f8da825f152613ed178561
4multimedia <biuro@4multimedia.pl>
2024-08-07 21:21:13 +0200

    1.0.26

--- src/index.js
+++ src/index.js
@@ -3,4 +3,25 @@ import DashboardPage from "./pages/dashboard/IndexPage.vue";
 import { AdminRoutes } from "./router/admin.js";
 import UpsoftInputText from "./components/input/Text.vue";
 
-export { AdminLayout, AdminRoutes, DashboardPage, UpsoftInputText };
+const components = {
+	AdminLayout,
+	DashboardPage,
+	UpsoftInputText,
+};
+
+const install = (app) => {
+	Object.keys(components).forEach((key) => {
+		app.component(key, components[key]);
+	});
+};
+
+export {
+	AdminLayout,
+	DashboardPage,
+	UpsoftInputText,
+	AdminRoutes,
+}
+
+export default {
+	install
+}
