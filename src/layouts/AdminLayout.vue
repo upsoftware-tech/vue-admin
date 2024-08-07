22eab7618e1410dd1ed75bca0aa901aa3dd8a1b2
4multimedia <biuro@4multimedia.pl>
2024-08-07 20:37:14 +0200

    1.0.25

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -3,11 +3,10 @@
 		<h1>Panel administracyjny</h1>
 		<router-view />
 		<upsoft-input-text />
-		<RouterView></RouterView>
+		<RouterView />
 	</div>
 </template>
 
 <script setup>
 import UpsoftInputText from './../components/input/Text.vue';
-import { RouterView } from "vue-router";
 </script>
